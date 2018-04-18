8251bfbee249de7335852f969688b1f1ea0ea46c
unknown <you@example.com>
2018-04-18 14:57:12 +0800

    add templates

--- week2/src/blog/views.py
+++ week2/src/blog/views.py
@@ -8,4 +8,5 @@ def index(request):
     # random_number = randint(1, 10)
     # return HttpResponse("Hello, world. {}".format(random_number))
     # return HttpResponse("Hello, world. You're at the blog index.")
-    return render(request, "index.html" {})
+    name = "Yubin"
+    return render(request, "index.html",{ "name" : name })
